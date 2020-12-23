e8267485cfd1f8b1085de6fbcc40e2abd6195a20
ianwijma <ian@wij.ma>
2020-12-23 14:06:47 +1100

    Added event bus

--- src/application.module.ts
+++ src/application.module.ts
@@ -6,10 +6,12 @@ import { ConfigurationModule } from './configuration/configuration.module';
 import { DatabaseModule } from './database/database.module';
 import { ElasticsearchModule } from './elasticsearch/elasticsearch.module';
 import { RedisModule } from './redis/redis.module';
+import { EventModule } from './event/event.module';
 
 @Module({
   imports: [
     ConfigurationModule,
+    EventModule,
     DatabaseModule,
     ElasticsearchModule,
     RedisModule,
